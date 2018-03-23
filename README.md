62e19be056448d70646d1e4d1f3aecfe8580c34d
Ryan <Admin@MacBook-Pro.local>
2018-03-23 14:30:04 -0400

    Giftastic

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Giftastic
+
+Week 6 Homework Assignment: Giftastic Movies
+
+•    I created an app to show gifs from your favorite movies
+•    Here is the link to my documentation: 
+•    Made sure all assigned tasks were done in a timely manner
+•    Helped to motivate cast members perform "magic moments"
+•    Ensured the safety of guests while performing ride operational duties
+•    Theme park knowledge as to better associate myself with guests
+
---
+++ assets/javascript/logic.js
@@ -0,0 +1,91 @@
+//global variables
+$(document).ready(function(){
+//An array of movie titles     
+var movies = ['american psycho', 'anchorman', 'harry potter', 'kill bill', 'scarface', 'spring breakers', 'star wars', 'the hunger games', 'the shinning','ace ventura', 'addams family', 'beetlejuice', 'bladerunner', 'dodgeball', 'elf', 'ghostbusters', 'indiana jones'];
+//Main Functions
+function displayGifbuttons (){
+    $('#Gifbuttonview').empty(); // erasing duplicate results in the div id
+    for (var i=0; i< movies.length; i++){
+    var gifButton = $("<button>");
+    gifButton.addClass("movies");
+    gifButton.addClass("btn btn-primary");
+    gifButton.attr("data-name", movies[i]);
+    gifButton.text(movies[i]);
+    $('#Gifbuttonview').append(gifButton);
+    }
+}
+
+//Function to add new movie button
+function AddnewButton(){
+    $("#addGif").on("click", function(){
+        var movie = $("#movie-input").val().trim();
+        if (movie == ""){
+            return false; // added so user cannot add a blank button
+        }
+        movies.push(movie);
+
+        displayGifbuttons();
+        return false;
+    });
+}
+//function to remove last movie button
+function RemovelastButton(){
+    $("removeGif").on("click", function(){
+        movies.pop(movie);
+        displayGifbuttons();
+        return false;
+    });
+}
+//function that displays all of the gifs
+function Displaygifs(){
+    var movie = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=F83wH8JtFWXAq94qIL3yXldOx8dTpEyN&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        console.log(response); // console test to make sure something returns
+        $("#gifsview").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
+        var results = response.data; //shows results of gifs
+        if (results == ""){
+          alert("There isn't a gif for this selected button");
+        }
+        for (var i=0; i<results.length; i++){
+
+            var gifDiv = $("<div>"); //div for the gifs to go inside
+            gifDiv.addClass("gifDiv");
+            // pulling rating of gif
+            var gifRating = $("<p>").text("Rating: " + results[i].rating);
+            gifDiv.append(gifRating);
+            // pulling gif
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+            gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
+            gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
+            gifImage.attr("data-state", "still"); // set the image state
+            gifImage.addClass("image");
+            gifDiv.append(gifImage);
+            // pulling still image of gif
+            // adding div of gifs to gifsView div
+            $("#gifsview").prepend(gifDiv);
+            }
+        });
+    }
+// Main Process
+displayGifbuttons();
+AddnewButton();
+RemovelastButton();
+// Document Event Listeners
+$(document).on("click", ".movies", Displaygifs);
+$(document).on("click", ".image", function(){
+    var state = $(this).attr('data-state');
+    if ( state == 'still'){
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    }else{
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+});
+});   
